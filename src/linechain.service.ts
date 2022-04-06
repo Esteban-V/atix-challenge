ac660156ec6ce9666bf3d8384b22cb7bd624fae7
Esteban Viera <esteban.viera.arroba@gmail.com>
2022-04-06 20:44:31 -0300

    - Remove static elements from linechain.service.ts

--- src/linechain.service.ts
+++ src/linechain.service.ts
@@ -9,84 +9,75 @@ export class LineChainService {
   private maxNonce = 500000; //Limiting nonce so that we don't calculate for so long
   private difficulty = 2; //Number of zeros in the beginning of the hash
 
-  private static fileDir = './output';
+  private fileDir = './output';
 
-  private static fileWriteStream: WriteStream;
-  private static lastLine: Line;
+  private fileWriteStream: WriteStream;
+  private lastLine: Line;
 
   constructor() {
-    console.log('LineChainService constructing...');
-    //Prevent initializing the writeStream more than once
-    if (!LineChainService.fileWriteStream) {
-      if (!existsSync(LineChainService.fileDir)) {
-        mkdirSync(LineChainService.fileDir);
-        console.log('Output directory created!');
-      } else if (existsSync(`${LineChainService.fileDir}/linechain.csv`)) {
-        readFile(
-          `${LineChainService.fileDir}/linechain.csv`,
-          'utf-8',
-          (err, data) => {
-            if (err) {
-              console.log(`Error while reading file: ${err}`);
-              throw err;
-            }
+    console.log('LineChainService initializing...');
 
-            const lines = data.trim().split('\n');
-            const lineChain = lines.map(
-              (line) =>
-                new Line({
-                  prevHash: line.split(',')[0],
-                  message: line.split(',')[1],
-                  nonce: line.split(',')[2],
-                }),
+    if (!existsSync(this.fileDir)) {
+      mkdirSync(this.fileDir);
+      console.log('Output directory created!');
+    } else if (existsSync(`${this.fileDir}/linechain.csv`)) {
+      readFile(`${this.fileDir}/linechain.csv`, 'utf-8', (err, data) => {
+        if (err) {
+          console.log(`Error while reading file: ${err}`);
+          throw err;
+        }
+
+        const lines = data.trim().split('\n');
+        const lineChain = lines.map(
+          (line) =>
+            new Line({
+              prevHash: line.split(',')[0],
+              message: line.split(',')[1],
+              nonce: line.split(',')[2],
+            }),
+        );
+
+        console.log(`Total lines in linechain: ${lineChain.length}`);
+
+        if (this.isLineChainValid(lineChain)) {
+          const lastLine = lines.slice(-1)[0];
+
+          if (!!lastLine) {
+            const [prevHash, message, nonce] = lastLine.split(',');
+            console.log(
+              `Loading last line from existing linechain: ${prevHash},${message},${nonce}`,
             );
 
-            console.log(`Total lines in linechain: ${lineChain.length}`);
+            this.setLastLine({
+              prevHash,
+              message,
+              nonce,
+            });
+          }
+        } else {
+          throw new Error('Linechain is not valid!');
+        }
+      });
+    }
 
-            if (this.isLineChainValid(lineChain)) {
-              const lastLine = lines.slice(-1)[0];
-
-              if (!!lastLine) {
-                const [prevHash, message, nonce] = lastLine.split(',');
-                console.log(
-                  `Loading last line from existing linechain: ${prevHash},${message},${nonce}`,
-                );
-
-                LineChainService.setLastLine({
-                  prevHash,
-                  message,
-                  nonce,
-                });
-              }
-            } else {
-              throw new Error('Linechain is not valid!');
-            }
-          },
-        );
-      }
-
-      LineChainService.fileWriteStream = createWriteStream(
-        `${LineChainService.fileDir}/linechain.csv`,
+    if (!this.fileWriteStream) {
+      this.fileWriteStream = createWriteStream(
+        `${this.fileDir}/linechain.csv`,
         { flags: 'a' },
       ).on('error', (err) => {
         console.log(`Error while writing to file: ${err}`);
       });
-
-      console.log('LineChainService initialized!');
     }
+    console.log('LineChainService initialized!');
   }
 
-  public static setLastLine({ prevHash, message, nonce }) {
-    LineChainService.lastLine = new Line({ prevHash, message, nonce });
+  public setLastLine({ prevHash, message, nonce }) {
+    this.lastLine = new Line({ prevHash, message, nonce });
     console.log(`Last line changed to ${prevHash},${message},${nonce}`);
   }
 
-  get lastLine(): Line {
-    return LineChainService.lastLine;
-  }
-
   writeMessage(message: string): boolean {
-    if (!LineChainService.fileWriteStream) return false; //Write stream not initialized
+    if (!this.fileWriteStream) return false; //Write stream not initialized
 
     const prevHash: string = this.lastLine
       ? this.lastLine.hash
@@ -99,11 +90,12 @@ export class LineChainService {
       throw new Error('Could not calculate a valid nonce!');
     }
 
-    LineChainService.setLastLine({ prevHash, message, nonce });
+    this.setLastLine({ prevHash, message, nonce });
 
     console.log(`Writing new line to file: ${prevHash},${message},${nonce}`);
 
-    LineChainService.fileWriteStream.write(`${prevHash},${message},${nonce}\n`);
+    this.fileWriteStream.write(`${prevHash},${message},${nonce}\n`);
+
     return true;
   }
 
